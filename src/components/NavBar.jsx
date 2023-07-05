29a82e111a983826f0cc11a1d6f35ecc5ccfa8f6
EmilyJarecki <jareckiemily@yahoo.com>
2023-07-05 12:59:31 -0500

    dropdown takes up the entire screen. able to close and open via usestate

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,38 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faX } from '@fortawesome/free-solid-svg-icons';
 
 export const NavBar = () => {
-const [count, setCount] = useState(0)
-
-  return (
-    <div>
-        <p>You have clicked {count} spots.</p>
-<button onClick={()=> setCount(count+1)}>move 1 spot</button>
-    </div>
-  )
-}
+    const [count, setCount] = useState(0);
+    const [dropdownOpen, setDropdownOpen] = useState(false);
+  
+    const toggleDropdown = () => {
+      setDropdownOpen(!dropdownOpen);
+    };
+  
+    const closeDropdown = () => {
+      setDropdownOpen(false);
+    };
+  
+    return (
+      <div>
+        <div>
+          <FontAwesomeIcon icon={faBars} onClick={toggleDropdown} />
+          {dropdownOpen && (
+            <div className="fixed inset-0 flex flex-col justify-between bg-slate-700 z-10">
+              <FontAwesomeIcon style={{color: "#ffffff"}} className="z-20" icon={faX} onClick={closeDropdown} />
+              <div className="flex flex-grow justify-center items-center z-20">
+                <ul className="space-y-2 text-white text-3xl py-5">
+                  <li className='py-5'>Report My Test</li>
+                  <li className='py-5'>FAQs</li>
+                  <li className='py-5'>About Us</li>
+                  <li className='py-5'>Resources</li>
+                </ul>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    );
+  };
+  

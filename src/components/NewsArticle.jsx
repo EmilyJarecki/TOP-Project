9c40397770d87b268a5db86e7d77028bf22a34b6
Mary Lueder <mlueder@gmail.com>
2023-07-06 13:42:26 -0400

    stub up news article component

---
+++ src/components/NewsArticle.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const NewsArticle = () => {
+  return (
+    <div>
+      <h1>Article</h1>
+    </div>
+  )
+}
+
+export default NewsArticle
--- src/components/Newsroom.jsx
+++ src/components/Newsroom.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import NewsArticle from './NewsArticle'
 
 const Newsroom = () => {
   return (
     <>
       <h1>Newsroom</h1>
+      <p>The latest news and media coverage of MakeMyTestCount.</p>
+      
+      <NewsArticle />
+      <NewsArticle />
+      <NewsArticle />
+      <NewsArticle />
+      <NewsArticle />
     </>
   )
 }

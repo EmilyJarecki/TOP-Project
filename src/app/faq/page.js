4997d0fa0dbb7d533f49230f91ab86fe7e64188c
sheikdon <sheikdonabdiresac@gmail.com>
2023-07-06 14:02:55 -0500

    test

--- src/app/faq/page.js
+++ src/app/faq/page.js
@@ -1,8 +1,20 @@
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+
+
+
 export default function Faq() {
 
     return (
         <>
+        <Header />
         <h1>FAQ Page</h1>
+        <a className="underline text-blue-500" href="/">Go BACK</a>
+        <center>
+        <p className="font-rubik font-bold text-xl text-hych-title-blue leading-none tracking-tight">Frequently Asked</p>
+        <p className="font-rubik font-bold text-xl text-hych-title-blue leading-none tracking-tight">Questions</p>
+        </center>
+        <Footer />
         </>
 
     )

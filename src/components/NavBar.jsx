58f8c905d6bed078c739110c337f13588f4549a1
EmilyJarecki <jareckiemily@yahoo.com>
2023-07-07 11:31:19 -0500

    routed pages for nav menu

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
+import Link from 'next/link';
 
 export const NavBar = () => {
     const [dropdownOpen, setDropdownOpen] = useState(false);
@@ -18,16 +19,17 @@ export const NavBar = () => {
           <FontAwesomeIcon size="2xl" style={{color: "#243558" }} icon={faBars} onClick={toggleDropdown} />
           {dropdownOpen && (
             <div className="fixed inset-0 flex flex-col justify-between bg-hych-subheading-blue z-10">
-                <div className="flex justify-end p-8">
-                <FontAwesomeIcon style={{color: "#ffffff"}} size="2xl" className="z-20" icon={faXmark} onClick={closeDropdown} />
+                <div className="flex justify-end">
+                <FontAwesomeIcon style={{color: "#ffffff"}} size="2xl" className="z-20 px-6 pt-12 pb-4" icon={faXmark} onClick={closeDropdown} />
 
                 </div>
               <div className="flex flex-grow justify-center items-center z-20">
                 <ul className="space-y-2 text-white text-3xl py-5">
-                  <li className='py-5'>Report My Test</li>
-                  <li className='py-5'>FAQs</li>
-                  <li className='py-5'>About Us</li>
-                  <li className='py-5'>Resources</li>
+                  {/* where does this go? */}
+                  <li className='py-5'>Report My Test</li> 
+                  <li className='py-5'><Link href="/faq">FAQs</Link></li>
+                  <li className='py-5'><Link href="/about">About Us</Link></li>
+                  <li className='py-5'><Link href="/resources">Resources</Link></li>
                 </ul>
               </div>
             </div>

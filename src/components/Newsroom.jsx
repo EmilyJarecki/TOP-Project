9652159e975fcba23551b48662746187406e217d
Mary Lueder <mlueder@gmail.com>
2023-07-08 13:59:12 -0400

    newsroom text

--- src/components/Newsroom.jsx
+++ src/components/Newsroom.jsx
@@ -5,8 +5,8 @@ import ArticleList from './ArticlesList'
 const Newsroom = () => {
   return (
     < >
-      <p className='text-center text-hych-subheading-blue'>Newsroom</p>
-      <p className='text-center text-hych-subheading-blue'>The latest news and media coverage of MakeMyTestCount.</p>
+      <p className='text-center text-hych-subheading-blue text-lg mb-3'>Newsroom</p>
+      <p className='text-center text-hych-subheading-blue text-xs mb-7'>The latest news and media coverage of MakeMyTestCount.</p>
       <ArticleList />
     </>
   )

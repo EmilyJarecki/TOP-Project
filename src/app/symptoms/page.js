1458a792c56d144461f57b53cde8ba08cce2f07d
xcbhx <ceina.ellison@gmail.com>
2023-07-08 18:56:14 -0400

    Imported Header/Footer

--- src/app/symptoms/page.js
+++ src/app/symptoms/page.js
@@ -1,11 +1,15 @@
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 import PickSymptoms from "@/components/PickSymptoms"
 
 export default function Symptoms() {
 
     return (
         <>
+            <Header />
             <PickSymptoms />
             <a className="underline text-blue-500" href="/">Go BACK</a>
+            <Footer/>
         </>
 
     )

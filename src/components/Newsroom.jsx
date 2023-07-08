b8a4fb5eb2abf3cc8f9ec2e6c40df861b02f7e32
Mary Lueder <mlueder@gmail.com>
2023-07-08 14:04:15 -0400

    font weight adjustment

--- src/components/Newsroom.jsx
+++ src/components/Newsroom.jsx
@@ -5,7 +5,7 @@ import ArticleList from './ArticlesList'
 const Newsroom = () => {
   return (
     < >
-      <p className='text-center text-hych-subheading-blue text-lg mb-3'>Newsroom</p>
+      <p className='text-center text-hych-subheading-blue text-lg mb-3 font-medium'>Newsroom</p>
       <p className='text-center text-hych-subheading-blue text-xs mb-7'>The latest news and media coverage of MakeMyTestCount.</p>
       <ArticleList />
     </>

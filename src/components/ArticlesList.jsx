35a7f268bf78c5b46bb1e0d78a88c3c0cb8b013e
Mary Lueder <mlueder@gmail.com>
2023-07-08 13:53:47 -0400

    article div border

--- src/components/ArticlesList.jsx
+++ src/components/ArticlesList.jsx
@@ -14,25 +14,25 @@ const ArticleList = () => {
   return (
     <div className='flex justify-center items-center'>
       <div className="grid grid-cols-2 gap-x-7 gap-y-4">
-        <div className="h-56 w-32 shadow-lg">
+        <div className="h-56 w-32 shadow-xl border-[1px] border-gray-50">
           <Image src={abc} alt="abc article"></Image>
         </div>
-        <div className="h-56 w-32 shadow-lg ">
+        <div className="h-56 w-32 shadow-xl border-[1px] border-gray-50">
           <Image src={cbs} alt="cbs article"></Image>
         </div>
-        <div className="h-56 w-32 shadow-lg ">
+        <div className="h-56 w-32 shadow-xl border-[1px] border-gray-50">
           <Image src={nih} alt="nih article"></Image>
         </div>
-        <div className="h-56 w-32 shadow-lg ">
+        <div className="h-56 w-32 shadow-xl border-[1px] border-gray-50">
           <Image src={time} alt="time article"></Image>
         </div>
-        <div className="h-56 w-32 shadow-lg ">
+        <div className="h-56 w-32 shadow-xl border-[1px] border-gray-50">
           <Image src={verywell} alt="verywell article"></Image>
         </div>
-        <div className="h-56 w-32 shadow-lg ">
+        <div className="h-56 w-32 shadow-xl border-[1px] border-gray-50">
           <Image src={marketwatch} alt="market watch article"></Image>
         </div>
-        <div className="h-56 w-32 shadow-lg ">
+        <div className="h-56 w-32 shadow-xl border-[1px] border-gray-50">
           <Image src={whitehouse} alt="white house article"></Image>
         </div>
       </div>

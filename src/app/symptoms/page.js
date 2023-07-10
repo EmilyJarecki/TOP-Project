77004b5bdb488b09e3d0a2135ff6c4d0f8ea2392
xcbhx <ceina.ellison@gmail.com>
2023-07-10 11:16:06 -0400

    Form structure setup

--- src/app/symptoms/page.js
+++ src/app/symptoms/page.js
@@ -1,6 +1,7 @@
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import PickSymptoms from "@/components/PickSymptoms"
+import FormSymptoms from "@/components/FormSymptoms";
 
 export default function Symptoms() {
 
@@ -8,6 +9,7 @@ export default function Symptoms() {
         <>
             <Header />
             <PickSymptoms />
+            <FormSymptoms />
             <a className="underline text-blue-500" href="/">Go BACK</a>
             <Footer/>
         </>

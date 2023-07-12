60d25c85ff8dafb666e98e363b0b76179d96ef73
camaya21 <cindymamaya21@gmail.com>
2023-07-12 17:41:21 -0400

    Added shadow on the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,7 @@ import logo from "../assets/logo.png";
 const Header = () => {
 
   return (
-    <div className="flex justify-between px-6 pt-12 pb-4">
+    <div className="flex justify-between px-6 pt-12 pb-4 shadow-xl">
       <div>
         <Image className="w-20" src={logo} alt="makemytestcount.org"></Image>
       </div>

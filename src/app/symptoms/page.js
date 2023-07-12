f39e487fc457de336fde17fbfe23b4be7e6be5b6
camaya21 <cindymamaya21@gmail.com>
2023-07-11 22:00:09 -0400

    Created symptomsData from symptoms images

--- src/app/symptoms/page.js
+++ src/app/symptoms/page.js
@@ -7,13 +7,12 @@ export default function Symptoms() {
 
     return (
         <>
-            <div className="bg-[#E4EAEE]">
-                <Header />
-                <PickSymptoms />
-                <AdditionalResourcesCarousel />
-                <Footer/>
-            </div>
-            
+            <Header />
+            <PickSymptoms />
+            <FormSymptoms />
+            <PopUpResults />
+            <a className="underline text-blue-500" href="/">Go BACK</a>
+            <Footer/>
         </>
 
     )

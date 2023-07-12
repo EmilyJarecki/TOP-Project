5a3dd0b955ff5e6d135c8a008190888b9edd16a4
camaya21 <cindymamaya21@gmail.com>
2023-07-11 21:04:54 -0400

    imported all symptoms images into image.js

--- src/assets/symptoms/images.js
+++ src/assets/symptoms/images.js
@@ -1,1 +1,28 @@
 import Abdominal from "./abdominal.png"
+import Breathing from "./breathing.png"
+import Chills from "./chills.png"
+import Cough from "./cough.png"
+import Diarrhea from "./diarrhea.png"
+import Fatigue from "./fatigue.png"
+import Fever from "./fever.png"
+import Headache from "./headache-art.png"
+import Muscle from "./muscle.png"
+import Nausea from "./nausea.png"
+import Nose from "./nose.png"
+import Rash from "./rash.png"
+
+export default {
+    abdominal: Abdominal,
+    breathing: Breathing,
+    chills: Chills,
+    cough: Cough,
+    diarrhea: Diarrhea,
+    fatigue: Fatigue,
+    fever: Fever,
+    headache: Headache,
+    muscle: Muscle,
+    nausea: Nausea,
+    nose: Nose,
+    rash: Rash,
+}
+

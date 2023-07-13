b241b58399c70fd0e8953bd4cbb7c30303b8b600
Mary Lueder <mlueder@gmail.com>
2023-07-13 15:15:13 -0400

    fix spacing on footer white space, remove Hello World from header

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,8 @@ import Link from 'next/link';
 import Footer from '@/components/Footer';
 import { Inter, Rubik } from 'next/font/google';
 // import Landing from '@/components/Landing'
-import map from '../assets/map.png'
+import map from "../assets/map.png"
+import HowYouCanHelp from '@/components/HowYouCanHelp';
 
 
 import AdditionalResourcesCarousel from '@/components/AdditionalResourcesCarousel';
@@ -20,7 +21,7 @@ export default function Home() {
       <Header />
       {/* <Landing /> */}
       <div className="flex flex-col items-center justify-center ">
-        <p className='bg-[#5B83CB] text-[#FAFBFF] text-[11.25px] mt-[42px] mb-[24px] py-[4px] px-[12px] rounded-full'>12,345 positive tests today</p>
+        <p className="bg-[#5B83CB] text-[#FAFBFF] text-[11.25px] mt-[42px] mb-[24px] py-[4px] px-[12px] rounded-full">12,345 positive tests today</p>
         <Image src={map} alt='map of the united states'></Image>
       </div>
       <div className='flex flex-col items-center justify-center font-bold text-[#30528F]' >
@@ -34,8 +35,9 @@ export default function Home() {
           <button className='shadow-md rounded-xl px-20 my-3'>Take a Photo to be Sure!</button>
         {/* </Link> */}
       </div>
-      <p className='text-center text-[#243C6C] text-[20px] font-bold font-inter mt-[52px]'>Why Your Test Results Matter</p>
-      <p className='text-[#385FA4] text-[11.5px] font-bold p-[9px] mx-[50px] mt-[20px] mb-[75px] rounded-[11px] shadow-md'>Your reports provide valuable information about patterns and trends related to COVID-19 symptoms, disease severity, and treatment outcomes. These insights enable us to refine our understanding of the virus, identify new variants, and adapt our strategies accordingly</p>
+      <p className="text-center text-[#243C6C] text-[20px] font-bold font-inter mt-[52px]">Why Your Test Results Matter</p>
+      <p className="text-[#385FA4] text-[11.5px] font-bold p-[9px] mx-[50px] mt-[20px] mb-[75px] rounded-[11px] shadow-md">Your reports provide valuable information about patterns and trends related to COVID-19 symptoms, disease severity, and treatment outcomes. These insights enable us to refine our understanding of the virus, identify new variants, and adapt our strategies accordingly</p>
+      <HowYouCanHelp></HowYouCanHelp>
       <AdditionalResourcesCarousel></AdditionalResourcesCarousel>
       <Footer />
     </main>

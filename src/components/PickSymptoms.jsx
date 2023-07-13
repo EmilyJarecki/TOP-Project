4cb47225bdc5c7ab3a64f5f8dd34d22101a36881
camaya21 <cindymamaya21@gmail.com>
2023-07-13 11:20:29 -0400

    Added working code

--- src/components/PickSymptoms.jsx
+++ src/components/PickSymptoms.jsx
@@ -1,5 +1,21 @@
-import React from 'react';
-import Image from 'next/image';
+import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+
+import Abdominal from "../assets/symptoms/abdominal.png"
+import Breathing from "../assets/symptoms/breathing.png"
+import Chills from "../assets/symptoms/chills.png"
+import Cough from "../assets/symptoms/cough.png"
+import Diarrhea from "../assets/symptoms/diarrhea.png"
+import Fatigue from "../assets/symptoms/fatigue.png"
+import Fever from "../assets/symptoms/fever.png"
+import Headache from "../assets/symptoms/headache-art.png"
+import Muscle from "../assets/symptoms/muscle.png"
+import Nausea from "../assets/symptoms/nausea.png"
+import Nose from "../assets/symptoms/nose.png"
+import Rash from "../assets/symptoms/rash.png"
+import Conjunctivitis from "../assets/symptoms/conjunctivitis.png"
+import Other from "../assets/symptoms/other.png"
 
 const symptomsData = [
   {
@@ -68,8 +84,8 @@ function PickSymptoms() {
     </div>
     <div className='grid grid-cols-3'>
         {symptomsData.map((item) => (
-          <button>
-            <div grid grid-cols-2 gap-4 justify-center items-center>
+          <button key={item.id}>
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
               <Image
                 className="w-20 rounded-xl bg-white"
                 src={item.image}
@@ -83,8 +99,11 @@ function PickSymptoms() {
     <div className='flex items-center justify-center'>
       <Link href='/symptoms2'>
         <button className='rounded-xl mx-3 px-12 text-black bg-white'>Skip</button>
+      </Link>
+      <Link href='/symptoms2'>
         <button className="rounded-xl mx-3 px-12 text-white bg-[#30528F]">Continue</button>
-      </div>
+      </Link>
+    </div>
     </>
   )
 }

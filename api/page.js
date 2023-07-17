a8f8c5aed6d0f1e64401ebde2ff7d41dbd54298e
Alaysia Veal <alaysiaveal@gmail.com>
2023-07-17 17:44:40 -0400

    skeleton for backend

---
+++ api/page.js
@@ -0,0 +1,13 @@
+const dbConnect = require('../lib/page')
+
+const handler = async (req, res) => {
+  const { method } = req
+  switch (method) {
+    case 'GET': {
+      await dbConnect()
+      res.json({ ok: true })
+      break
+    }
+  }
+}
+module.exports = handler

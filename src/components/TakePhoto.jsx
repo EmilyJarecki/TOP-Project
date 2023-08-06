69459ef1580ea0224a2d72dcbffc9a03f7b4cef9
xcbhx <ceina.ellison@gmail.com>
2023-08-06 16:20:28 -0400

    Set up using css & added images

--- src/components/TakePhoto.jsx
+++ src/components/TakePhoto.jsx
@@ -1,6 +1,11 @@
 "use client"
 import React, { useRef, useState } from "react";
 import Webcam from "react-webcam";
+import Image from "next/image";
+import invalidTest from "../assets/takePhotos/invalidTest.png";
+import negativeTest from "../assets/takePhotos/negativeTest.png";
+import positiveTest from "../assets/takePhotos/positiveTest.png";
+
 
 export const TakePhoto = () => {
   const webcamRef = useRef(null);
@@ -12,17 +17,28 @@ export const TakePhoto = () => {
   };
 
   return (
-    <div className="flex flex-col items-center">
-      <Webcam audio={false} ref={webcamRef} screenshotFormat="image/jpeg" />
-      <button
-        className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        onClick={captureImage}
-      >
-        Take Photo
-      </button>
-      {capturedImage && (
-        <img className="mt-4" src={capturedImage} alt="Captured" />
-      )}
+    <div className="container text-center h-max">
+      <div className="flex flex-col items-center">
+        <p className="text-hych-title-blue font-bold">Compare your sample results to the current results below.</p>
+        <br />
+        <p className="text-hych-subheading-blue">Positive Result</p>
+        <Image src={positiveTest} alt="postivie covid-19 result"/>
+        <p className="text-hych-subheading-blue">Negative Result</p>
+        <Image src={negativeTest} alt="negative covid-19 result" />
+        <p className="text-hych-subheading-blue">Invalid Result</p>
+        <Image src={invalidTest} alt="invalid covid-19 result" />
+        <br />
+        <Webcam audio={false} ref={webcamRef} screenshotFormat="image/jpeg" />
+        <button
+          className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold px-4 rounded"
+          onClick={captureImage}
+        >
+          Take Photo
+        </button>
+        {capturedImage && (
+          <img className="mt-4" src={capturedImage} alt="Captured" />
+        )}
+      </div>
     </div>
   );
 };
